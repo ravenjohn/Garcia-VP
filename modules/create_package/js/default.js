3e16b6246e860d027bf7f0f5384bee9263b5e7df
karla <kpalevino@gmail.com>
2013-02-25 10:55:30 +0800

    added create package module. signed-off-by: karla<kpalevino@gmail.com>

--- modules/create_package/js/default.js
+++ modules/create_package/js/default.js
@@ -12,15 +12,7 @@ function create_package(f){
 		function (data, textStatus, jqXHR){
 			if(jqXHR.status==200){
 				data = JSON.parse(data);
-				$(".remove").remove();
-				$('#view_packages_table').append("					
-					<tr>
-						<td>"+ f.name.value +"</td>
-						<td>"+ f.cost.value +"</td>
-					</tr>
-				");
-				f.reset();
-				f.name.focus();
+				console.log(data);
 				alert(data.message);
 			}else{
 				console.log(jqXHR);
