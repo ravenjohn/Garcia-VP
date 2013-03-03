1c4e7906ed81cc522ef1f2b9de1df9348981479e
ravenjohn <rjlagrimas08@gmail.com>
2013-03-03 21:22:48 +0800

    Fixed main menu, added profile navigation. signed-off-by: Raven Lagrimas <rjlagrimas08@gmail.com>

--- modules/logout/tmpl/index.php
+++
@@ -1,5 +0,0 @@
-<div id="logout_div" class="mod admin_module user_module">
-	<form method="POST" action="<?php echo RConfig::ajax_url?>logout" onsubmit="return logout(this);">
-		<input type="submit" value="logout" class="btn"/>
-	</form>
-</div>
